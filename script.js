d6d941de9191b012ceda334b47ccca7abe7edbd2
Himanshu choudhary <86661174+Iamhc@users.noreply.github.com>
2024-10-04 23:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function nextStep() {
+  document.getElementById("step1Form").style.display = "none";
+  document.getElementById("step2Form").style.display = "block";
+}
+
+function submitForm() {
+  // You can handle form submission here, e.g., sending data to a server or displaying a confirmation message
+  alert("Form submitted successfully!");
+}
